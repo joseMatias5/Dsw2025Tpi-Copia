c65a2698d248d00ee21be0ff1ff2b9ee1ae74fcf
josematias5 <jomatias108@gmail.com>
2025-06-22 10:50:53 -0300

    Clases Model creadas

---
+++ Dsw2025Tpi.Application/Dtos/OrderModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Dtos;
+
+public record OrderModel
+{
+    public record Request(DateTime Date, string ShippingAddress, string BillingAddress, string? Notes, decimal TotalAmount);
+    public record Response(Guid Id);
+
+}
