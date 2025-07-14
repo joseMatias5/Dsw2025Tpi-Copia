98db1a4be18d1be72e7dc5c7a00fefc59a6bea9e
josematias5 <jomatias108@gmail.com>
2025-07-14 16:33:13 -0300

    Domain hecho y corregido

---
+++ Dsw2025Tpi.Domain/Entities/OrderItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities;
+
+public class OrderItem : EntityBase
+{
+    public int? Quantity { get; set; }
+    public decimal? UnitPrice { get; set; }
+    public decimal? SubTotal => Quantity * UnitPrice;
+    public Guid ProductId { get; set; }
+
+    private Product _product;
+    public Product? Product
+    {
+        get=> _product;
+        set
+        {
+            if (Product?.IsActive == false)
+            {
+                throw new InvalidOperationException("Product has to be active");
+            }
+            _product = value;
+        }
+    }
+    public Guid OrderId { get; set; }
+    public Order? Order { get; set; }
+
+    public OrderItem(int quantity, Product product)
+    {
+        Quantity = quantity < 0? 
+            throw new ArgumentOutOfRangeException(nameof(quantity), "The Unit Price must be positive") : 
+            Product.StockControl(quantity)? quantity : throw new ArgumentException(nameof(quantity), "Stock problems");
+        UnitPrice = Product?.CurrentUnitPrice;
+        Product = product ?? throw new ArgumentNullException(nameof(product), "Product cannot be null");
+    }
+}
---
+++ Dsw2025Tpi.Domain/Entities/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities;
+
+public class Product : EntityBase
+{
+   
+    public string? Sku { get; set; }
+    public string? InternalCode { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal? CurrentUnitPrice { get; set; }
+    public int? StockQuantity {  get; set;}
+    public bool? IsActive { get; set; }
+    public ICollection<OrderItem>? OrderItems { get; set; }
+
+    public Product(string? sku, string? internalCode, string? name, string? description, 
+        decimal? currentUnitPrice, int? stockQuantity)
+    {
+        Sku = sku;
+        InternalCode = internalCode;
+        Name = name;
+        Description = description;
+        CurrentUnitPrice = currentUnitPrice <= 0? 
+            throw new ArgumentOutOfRangeException(nameof(stockQuantity), "The Unit Price must be positive") : currentUnitPrice;
+        StockQuantity = stockQuantity <= 0 ?
+            throw new ArgumentOutOfRangeException(nameof(stockQuantity), "The Quantity must be positive") : stockQuantity;
+        IsActive = true;
+    }
+    public bool StockControl(int quantity)
+    {
+        if (quantity < StockQuantity)
+        {
+            StockQuantity -= quantity;
+            return true;
+        }
+        return false;
+    }
+}
